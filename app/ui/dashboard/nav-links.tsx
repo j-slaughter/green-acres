eae4bde806a59c6c33d9190d2acddeef657b9bef
Jennifer Slaughter <129903316+j-slaughter@users.noreply.github.com>
2025-04-30 16:03:11 -0700

    add active link styling

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,11 @@
+// This is the dashboard sidebar navigation links
+// Add "use client" to turn nav-links.tsx into a Client Component to use usePathname React hook
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -13,19 +20,24 @@ const links = [
 ];
 
 export default function NavLinks() {
+  // Get user's current path from URL
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-lime-100 hover:text-green-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-lime-100 hover:text-green-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              { 'bg-lime-100 text-green-600': pathname === link.href }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
